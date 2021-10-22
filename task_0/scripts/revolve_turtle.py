bda32554898c54655ec1d2457ba1e924b8fb504b
DeepBlueS3a <33351122+DeepBlueS3a@users.noreply.github.com>
2021-10-22 21:36:41 +0530

    Update revolve_turtle.py

--- task_0/scripts/revolve_turtle.py
+++ task_0/scripts/revolve_turtle.py
@@ -19,31 +19,33 @@ def set_movement(lin, ang):
     move_node.publish(vel_msg)
 
 def move_bot():
-    speed = float(input('enter the speed you want it to move with : '))
-    distance = float(input('enter the radius of the revolutions you want : '))
-    no_revs = float(input('enter the number of revolutions you want : '))
-    time_ = float(distance)/float(speed) # the amount of time the robot should travel for
-    
-
-    set_movement(speed, 0)
-    time.sleep(time_)
-    set_movement(0, 0)
-
+    speed = 0.7
+    distance = 0.75
+    no_revs = 1
 # Now to turn 90 degrees
     set_movement(0, speed)
+
     time.sleep(ninety/speed)
     set_movement(0, 0)    
 
-#    no_revs = 1
 
     time_1 = no_revs * 2 * math.pi * distance / speed
     ang_vel = 2 * math.pi / time_1 * no_revs
-    print(f'itll move for {time_1} seconds')
+    print(f'itll move for {time_1*2} seconds')
  
     start = time.time()   
     t = False
     while not t:
         set_movement(speed, ang_vel)
+#        time.sleep(time_1)
+        if time.time() - start >= time_1:
+            t = True
+            set_movement(0, 0)
+
+    start = time.time()   
+    t = False
+    while not t:
+        set_movement(speed, -ang_vel)
 #        time.sleep(time_1)
         if time.time() - start >= time_1:
             t = True
